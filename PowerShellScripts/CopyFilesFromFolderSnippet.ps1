d21cb0afee68284a034e935ef8e832b30b6777d2
Nezam Ahamed <NezamAhamed87@users.noreply.github.com>
2021-11-29 11:37:05 -0500

    First commit

---
+++ PowerShellScripts/CopyFilesFromFolderSnippet.ps1
@@ -0,0 +1,16 @@
+﻿$ArrayOfFiles = '','','';
+
+
+$fileDirectory = "T:\08102021\";
+$destinationDirectory = "C:\Nezam\08102021\";
+
+foreach($documentName in Get-ChildItem $fileDirectory)
+{
+    $documentPath = $fileDirectory + $documentName;
+
+    if($ArrayOfFiles -contains $documentName)
+    {
+        Write-Output $documentName;
+        Copy-Item -Path $documentPath -destination $destinationDirectory
+    }
+}
